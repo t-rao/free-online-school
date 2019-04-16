4bb45203b23d46a338b82a9f2436afc577ab56f6
Trivikram244 <trivikram244@gmail.com>
2019-04-16 09:09:39 +0530

    Done
    Done

--- ajax/contact.php
+++ ajax/contact.php
@@ -1,22 +1,110 @@
 <?php
-    if(isset($_POST['name'], $_POST['email'])){
-        
-        $name = $_POST['name'];
-        $email= $_POST['email'];
+    // if(isset($_POST['name'], $_POST['email'])){
 
-        $to ='kent.shikano@gmail.com';
-        
-        $subject = 'Form Submission';
-        $message = "Name: ". $name . "\n" . "Email: ".$email;
-        $headers = "From: ". $email;
+        // $name = $_POST['name'];
+        // $email= $_POST['email'];
 
 
-        if(mail($to, $subject, $message, $headers)){
-            echo "1" ;
+        // if (empty($_POST["name"])) {
+        //     // $nameError = "Name is required";
+        //     echo "Name is required";
+        //     } else {
+        //     $name = $_POST["name"];
+        //     // check name only contains letters and whitespace
+        //     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+        //     // $nameError = "Only letters and white space allowed";
+        //     echo "Only letters and white space allowed";
+        //     }
+        //     }
+
+
+        //     if (empty($_POST["email"])) {
+        //     // $emailError = "Email is required";
+        //     echo "Email is required";
+        //     } else {
+        //     $email = $_POST["email"];
+        //     // check if e-mail address syntax is valid or not
+        //     if (!preg_match("/([w-]+@[w-]+.[w-]+)/",$email)) {
+        //     // $emailError = "Invalid email format";
+        //     echo "Invalid email format";
+
+        //     }
+        // }
+
+        // $to ='kent.shikano@gmail.com';
+    //     $to ='trivikram244@gmail.com';
+    //
+    //
+    //     $subject = 'お問い合わせがありました。「大富豪の学校」';
+    //     $message = "お名前: ". $name . "\n" . "Email: ".$email;
+    //     $headers = "メールアドレス: ". $email;
+    //
+    //
+    //     if(mail($to, $subject, $message, $headers)){
+    //         echo "1" ;
+    //     }
+    //     else{
+    //         echo "0";
+    //     }
+    //
+    // }
+
+
+    // Only process POST reqeusts.
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Get the form fields and remove whitespace.
+            $name = strip_tags(trim($_POST["name"]));
+    				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+            $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+            // $message = trim($_POST["message"]);
+
+            // Check that data was sent to the mailer.
+            if ( empty($name)  OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                // Set a 400 (bad request) response code and exit.
+                http_response_code(400);
+                echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+                exit;
+            }
+
+            // Set the recipient email address.
+            // FIXME: Update this to your desired email address.kent.shikano@gmail.com
+            $recipient = "sample@sample.com";
+
+            // Set the email subject.
+            $subject = "お問い合わせがありました。「大富豪の学校";
+
+            // Build the email content.
+            $email_content = "お名前: $name\n";
+            $email_content .= "Email: $email\n\n";
+
+            // Build the email headers.
+            $email_headers = "メールアドレス: $name <$email>";
+
+            // Send the email.
+            if (mail($recipient, $subject, $email_content, $email_headers)) {
+                // Set a 200 (okay) response code.
+                http_response_code(200);
+                echo "Thank You! Your message has been sent.";
+            } else {
+                // Set a 500 (internal server error) response code.
+                http_response_code(500);
+                echo "Oops! Something went wrong and we couldn't send your message.";
+            }
+
+        } else {
+            // Not a POST request, set a 403 (forbidden) response code.
+            http_response_code(403);
+            echo "There was a problem with your submission, please try again.";
         }
-        else{
-            echo "0";
-        }
-       
-    }
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- js/main.js
+++ js/main.js
@@ -1,34 +1,114 @@
-$('form.news_letter').on('submit', function(){
 
-    var that = $(this),
-    url = that.attr('action'),
-    type = that.attr('method'),
-    data ={};
+$(function() {
+  //get the form
+  var form = $("#form-01");
+  //Get the message Div
+  var formMessages = $("#form-messages");
+  $(form).submit(function(event){
+      event.preventDefault();
+      // serialize the for data
+      var formData = $(form).serialize();
 
-that.find('[name]').each(function(index, value){
-var that = $(this),
-name = that.attr('name'),
-value = that.val();
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    })
+    .done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
 
-data[name]=value;
+        // Set the message text.
 
+        $("#myModal").modal();
+        $(formMessages).text(response);
+        $("#form-01").trigger("reset");
+        // Clear the form.
+
+        $('#name1').val('');
+        $('#email1').val('');
+
+        setTimeout(function() {
+            $('#myModal').modal('hide');
+        }, 5000);
+    })
+    .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+        $("#myModal").modal();
+        $("#form-01").trigger("reset");
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+        } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+
+        setTimeout(function() {
+            $('#myModal').modal('hide');
+        }, 5000);
+    });
+
+
+  })
 });
 
 
 
-$.ajax({
-url:url,
-type:type,
-data:data,
-success:function(response){
-    if(response == 1){
-        $(".news_letter"). trigger("reset")
-        alert("Form submitted success fully");
-    }else{
-        alert("Some thing went wrong, please try again");
-    }
-}
-});
+$(function() {
+  //get the form
+  var form = $("#form-02");
+  //Get the message Div
+  var formMessages = $("#form-messages");
+  $(form).submit(function(event){
+      event.preventDefault();
+      // serialize the for data
+      var formData = $(form).serialize();
 
-return false;
-})
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    })
+    .done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+
+        // Set the message text.
+
+        $("#myModal").modal();
+        $(formMessages).text(response);
+        $("#form-02").trigger("reset");
+        // Clear the form.
+
+        $('#name').val('');
+        $('#email').val('');
+
+        setTimeout(function() {
+            $('#myModal').modal('hide');
+        }, 5000);
+    })
+    .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+        $("#myModal").modal();
+        $("#form-02").trigger("reset");
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+        } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+
+        setTimeout(function() {
+            $('#myModal').modal('hide');
+        }, 5000);
+    });
+
+
+  })
+});
